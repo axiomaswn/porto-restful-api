3fa733bd966886dd8b670e54a24894bcd22f2154
YOMA <email": null>
2017-02-19 17:55:59 +0700

    membuat tampilan dasar pada index.html

--- server/app.js
+++ server/app.js
@@ -4,10 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var color = require('./routes/color');
 const cors = require('cors');
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/artikel');
+
 var app = express();
 
 // view engine setup
@@ -21,9 +24,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors())
+app.use(cors());
 app.use('/', index);
-app.use('/color', color);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var artikelController = require("../controllers/artikel")
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.post('/artikel', artikelController.create)
+
+router.get('/artikel', artikelController.find)
+
+router.put('/artikel/:id', artikelController.update)
+
+router.delete('/artikel/:id', artikelController.delete)
 
 module.exports = router;
